96368967cba6b514592b7b83d11f1c7ae3874465
Hannah Elliott <hgelliott90@gmail.com>
2020-11-23 21:20:59 -0500

    Ready to test.

--- public/app.js
+++ public/app.js
@@ -12,11 +12,19 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true })); 
 
+// error function for api routes
+function error(err) {
+    if(err) throw err;
+    res.json(db);
+}
+
 // html routes
+// return notes.html
 app.get('/notes', function(req, res){
     res.sendFile(path.join(__dirname + '/public/', 'notes.html'));
 }); 
 
+// return index.html
 app.get('*', function(req, res){
     res.sendFile(path.join(__dirname + '/public/', 'index.html'));
 });
@@ -31,9 +39,27 @@ app.get('/api/notes', function(req, res){
 app.post('/api/notes', function(req, res){
     req.body.id = uuidv4();
     db.push(req.body);
-    fs.writeFile('./db/db.json', JSON.stringify(db), function(err){
-        if(err) throw err;
-        
-        res.json(db); 
-    });
+    fs.writeFile('./db/db.json', JSON.stringify(db), error());
 });
+
+// delete - receive query param containing unique id of note to delete.
+// read all notes from db.json, remove note with id, rewrite notes to db.json
+app.delete('/api/notes/all', function(req, res){
+    db.splice(0, db.length); 
+    fs.writeFile('./db/db.json', JSON.stringify(db), error());
+});
+
+app.delete('/api/notes/:id', function(req, res){
+    let id = req.params.id;
+    for(i = 0; i < db.length; i++) {
+        if (id === db[i].id) {
+            db.splice(i, 1);
+        }
+    }
+    fs.writeFile('./db/db.json', JSON.stringify(db), error());
+});
+
+// maybe a server thing
+app.listen(PORT, function(){
+    console.log('Application listening on PORT ' + PORT); 
+}); 
