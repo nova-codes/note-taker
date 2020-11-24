622c81afbcec74af7d880bd44f88e8451e7e28d7
Hannah Elliott <hgelliott90@gmail.com>
2020-11-23 22:23:27 -0500

    Debugging

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // requires and constants
 const express = require('express');
 const path = require('path');
-const db = require('./db/db');
+const db = require(__dirname + '/db/db.json');
 const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,31 +12,25 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true })); 
 
-// error function for api routes
-function error(err) {
-    if(err) throw err;
-    res.json(db);
-}
-
 // html routes
-// return notes.html
-app.get('/notes', function(req, res){
-    res.sendFile(path.join(__dirname + '/public/', 'notes.html'));
-}); 
-
 // return index.html
-app.get('*', function(req, res){
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/', 'index.html'));
 });
 
+// return notes.html
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/', 'notes.html'));
+}); 
+
 // api routes
 // get - read the db.json and return all saved notes as json
-app.get('/api/notes', function(req, res){
+app.get('/api/notes', (req, res) => {
     res.json(db); 
 });
 
 // post - receive new note to save, add to db.json, return new note to client
-app.post('/api/notes', function(req, res){
+app.post('/api/notes', (req, res) => {
     req.body.id = uuidv4();
     db.push(req.body);
     fs.writeFile('./db/db.json', JSON.stringify(db), function(err){
@@ -47,7 +41,7 @@ app.post('/api/notes', function(req, res){
 
 // delete - receive query param containing unique id of note to delete.
 // read all notes from db.json, remove note with id, rewrite notes to db.json
-app.delete('/api/notes/all', function(req, res){
+app.delete('/api/notes/all', (req, res) => {
     db.splice(0, db.length); 
     fs.writeFile('./db/db.json', JSON.stringify(db), function(err){
         if(err) throw err;
@@ -55,7 +49,7 @@ app.delete('/api/notes/all', function(req, res){
     });
 });
 
-app.delete('/api/notes/:id', function(req, res){
+app.delete('/api/notes/:id', (req, res) => {
     let id = req.params.id;
     for(i = 0; i < db.length; i++) {
         if (id === db[i].id) {
